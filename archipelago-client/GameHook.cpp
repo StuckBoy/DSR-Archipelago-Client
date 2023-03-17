05c3c10c54afccadc21d88c543059a4de37903c3
Zachary Stuck <zstuck96@gmail.com>
2023-03-17 15:45:23 -0400

    Add GameHook

---
+++ archipelago-client/GameHook.cpp
@@ -0,0 +1,362 @@
+#include "GameHook.h"
+
+DWORD64 qItemEquipComms = 0;
+
+DWORD64 rItemRandomiser = 0;
+DWORD64 rAutoEquip = 0;
+DWORD64 rNoWeaponRequirements = 0;
+DWORD64 rEquipLock = 0;
+
+LPVOID itemGibDataCodeCave;
+
+extern CItemRandomiser* ItemRandomiser;
+extern CArchipelago* ArchipelagoInterface;
+extern CCore* Core;
+
+/*
+* Check if a basic hook is working on this version of the game  
+*/
+BOOL CGameHook::preInitialize() {
+	if (MH_Initialize() != MH_OK) {
+		return false;
+	}
+	return Hook(0x1407BBA80, (DWORD64)&tItemRandomiser, &rItemRandomiser, 5);
+}
+
+BOOL CGameHook::initialize() {
+
+	BOOL bReturn = true;
+
+	//Inject ItemGibData
+	itemGibDataCodeCave = InjectShellCode(nullptr, ItemGibDataShellcode, 17);
+
+	//Modify ItemGibShellcode
+	bReturn &= replaceShellCodeAddress(ItemGibShellcode, 15, itemGibDataCodeCave, 0, sizeof(void*));
+	bReturn &= replaceShellCodeAddress(ItemGibShellcode, 26, itemGibDataCodeCave, 4, 4);
+	bReturn &= replaceShellCodeAddress(ItemGibShellcode, 33, itemGibDataCodeCave, 8, 4);
+
+	//Inject ItemGibShellcode
+	LPVOID itemGibCodeCave = InjectShellCode((LPVOID)0x13ffe0000, ItemGibShellcode, 93);
+
+	/*
+	TODO Implement options
+	if (dIsAutoEquip) {
+		bReturn &= Hook(0x1407BBE92, (DWORD64)&tAutoEquip, &rAutoEquip, 6);
+	}
+	if (dIsNoWeaponRequirements) { 
+		bReturn &= Hook(0x140C073B9, (DWORD64)&tNoWeaponRequirements, &rNoWeaponRequirements, 7); 
+	}
+	if (dIsNoSpellsRequirements) {
+		RemoveSpellsRequirements(); 
+	}
+	if (dLockEquipSlots) {
+		LockEquipSlots();
+	}
+	if (dIsNoEquipLoadRequirements) {
+		RemoveEquipLoad();
+	}
+	*/
+
+	return bReturn;
+}
+
+VOID CGameHook::manageDeathLink() {
+
+	if (lastHealthPoint == 0 && healthPoint != 0) {	//The player just respawned
+		deathLinkData = false;
+	} else if (deathLinkData && lastHealthPoint != 0 && healthPoint != 0 ) { //The player received a deathLink
+		killThePlayer();
+	} else if(lastHealthPoint != 0 && healthPoint == 0) { //The player just died, ignore the deathLink if received
+		if (deathLinkData) {
+			deathLinkData = false;
+			return;
+		}
+		ArchipelagoInterface->sendDeathLink();
+	}
+}
+
+VOID CGameHook::killThePlayer() {
+	DWORD processId = GetCurrentProcessId();
+	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, processId);
+	std::vector<unsigned int> hpOffsets = { 0x80, 0x1F90, 0x18, 0xD8 };
+	uintptr_t healthPointAddr = FindExecutableAddress(0x4768E78, hpOffsets); //BaseB + HP Offsets
+
+	int newHP = 0;
+	WriteProcessMemory(hProcess, (BYTE*)healthPointAddr, &newHP, sizeof(newHP), nullptr);
+}
+
+BOOL CGameHook::updateRuntimeValues() {
+	DWORD processId = GetCurrentProcessId();
+	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, processId);
+
+	std::vector<unsigned int> hpOffsets = { 0x80, 0x1F90, 0x18, 0xD8 };
+	uintptr_t healthPointAddr = FindExecutableAddress(0x4768E78, hpOffsets); //BaseB + HP Offsets
+
+	std::vector<unsigned int> playTimeOffsets = { 0xA4 };
+	uintptr_t playTimeAddr = FindExecutableAddress(0x4740178, playTimeOffsets); //BaseA + PlayTime Offsets	
+
+	std::vector<unsigned int> soulOfCinderDefeatedFlagOffsets = { 0x00, 0x5F67 };
+	uintptr_t soulOfCinderDefeatedFlagAddress = FindExecutableAddress(0x473BE28, soulOfCinderDefeatedFlagOffsets); //GameFlagData + Sould of Cinder defeated flag Offsets	
+
+	lastHealthPoint = healthPoint;
+
+	ReadProcessMemory(hProcess, (BYTE*)healthPointAddr, &healthPoint, sizeof(healthPoint), &healthPointRead);
+	ReadProcessMemory(hProcess, (BYTE*)playTimeAddr, &playTime, sizeof(playTime), &playTimeRead);
+	ReadProcessMemory(hProcess, (BYTE*)soulOfCinderDefeatedFlagAddress, &soulOfCinderDefeated, sizeof(soulOfCinderDefeated), &soulOfCinderDefeatedFlagRead);
+
+}
+
+VOID CGameHook::giveItems() {
+	//Send the next item in the list
+	if (!ItemRandomiser->receivedItemsQueue.empty()) {
+		itemGib(ItemRandomiser->receivedItemsQueue.back());
+	}
+}
+
+BOOL CGameHook::isSoulOfCinderDefeated() {
+	constexpr std::uint8_t mask7{ 0b1000'0000 };
+	return soulOfCinderDefeatedFlagRead != 0 && (int)(soulOfCinderDefeated & mask7) == 128;
+}
+
+VOID CGameHook::itemGib(DWORD itemId) {
+
+	DWORD processId = GetCurrentProcessId();
+	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, processId);
+
+	uintptr_t gibItem = (uintptr_t)itemGibDataCodeCave + 4;
+
+	char* littleEndianItemId = (char*)malloc(sizeof(DWORD));
+	ConvertToLittleEndianByteArray((uintptr_t)itemId, littleEndianItemId);
+
+	DWORD memory = 0;
+	ReadProcessMemory(hProcess, (BYTE*)gibItem, &memory, sizeof(memory), nullptr);
+	DWORD newMemory = itemId;
+	WriteProcessMemory(hProcess, (BYTE*)gibItem, &newMemory, sizeof(newMemory), nullptr);
+
+	typedef int func(void);
+	func* f = (func*)0x13ffe0000;
+	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)f, NULL, NULL, NULL);
+}
+
+BOOL CGameHook::Hook(DWORD64 qAddress, DWORD64 qDetour, DWORD64* pReturn, DWORD dByteLen) {
+
+	MH_STATUS status = MH_CreateHook((LPVOID)qAddress, (LPVOID)qDetour, 0);
+	if (status != MH_OK) return false;
+	if (MH_EnableHook((LPVOID)qAddress) != MH_OK) return false;
+
+	*pReturn = (qAddress + dByteLen);
+
+	return true;
+}
+
+BOOL CGameHook::replaceShellCodeAddress(BYTE *shellcode, int shellCodeOffset, LPVOID codeCave, int codeCaveOffset, int length) {
+
+	char* addressArray = (char*)malloc(sizeof(void*));
+	ConvertToLittleEndianByteArray((uintptr_t)codeCave + codeCaveOffset, addressArray);
+	if (addressArray == 0) { return false; }
+	memcpy(shellcode + shellCodeOffset, addressArray, length);
+	free(addressArray);
+
+	return true;
+}
+
+LPVOID CGameHook::InjectShellCode(LPVOID address, BYTE* shellCode, size_t len) {
+	
+	LPVOID pCodeCave = VirtualAlloc(address, 0x3000, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	if (!pCodeCave) {
+		return nullptr;
+	}
+
+	// copy the machine code into that memory:
+	std::memcpy(pCodeCave, shellCode, len);
+
+	// mark the memory as executable:
+	DWORD lpflOldProtect;
+	VirtualProtect(pCodeCave, len, PAGE_EXECUTE_READ, &lpflOldProtect);
+
+	return pCodeCave;
+}
+
+void CGameHook::ConvertToLittleEndianByteArray(uintptr_t address, char* output) {
+	for (int i = 0; i < sizeof(void*); ++i) {
+		output[i] = address & 0xff;
+		address >>= 8;
+	}
+}
+
+uintptr_t CGameHook::FindExecutableAddress(uintptr_t ptrOffset, std::vector<unsigned int> offsets) {
+	DWORD processId = GetCurrentProcessId();
+	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, processId);
+
+	uintptr_t moduleBase = GetModuleBaseAddress();
+	uintptr_t dynamicPtrAddr = moduleBase + ptrOffset;
+	return FindDMAAddy(hProcess, dynamicPtrAddr, offsets);
+}
+
+uintptr_t CGameHook::FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> offsets) {
+
+	uintptr_t addr = ptr;
+	for (unsigned int i = 0; i < offsets.size(); ++i) {
+		ReadProcessMemory(hProc, (BYTE*)addr, &addr, sizeof(addr), 0);
+		addr += offsets[i];
+	}
+	return addr;
+}
+
+uintptr_t CGameHook::FindDMAAddyStandalone(uintptr_t ptr, std::vector<unsigned int> offsets) {
+
+	DWORD processId = GetCurrentProcessId();
+	HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, NULL, processId);
+
+	uintptr_t addr = ptr;
+	for (unsigned int i = 0; i < offsets.size(); ++i) {
+		ReadProcessMemory(hProc, (BYTE*)addr, &addr, sizeof(addr), 0);
+		addr += offsets[i];
+	}
+	return addr;
+}
+
+uintptr_t CGameHook::GetModuleBaseAddress() {
+	const char* lpModuleName = "DarkSoulsIII.exe";
+	DWORD procId = GetCurrentProcessId();
+
+	MODULEENTRY32 lpModuleEntry = { 0 };
+	HANDLE hSnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, procId);
+	if (!hSnapShot)
+		return NULL;
+	lpModuleEntry.dwSize = sizeof(lpModuleEntry);
+	BOOL bModule = Module32First(hSnapShot, &lpModuleEntry);
+	while (bModule)
+	{
+		if (!strcmp(lpModuleEntry.szModule, lpModuleName))
+		{
+			CloseHandle(hSnapShot);
+			return (uintptr_t)lpModuleEntry.modBaseAddr;
+		}
+		bModule = Module32Next(hSnapShot, &lpModuleEntry);
+	}
+	CloseHandle(hSnapShot);
+	return NULL;
+}
+
+VOID CGameHook::LockEquipSlots() {
+
+	DWORD dOldProtect = 0;
+	DWORD64 qEquip = 0x140B70F45;
+	DWORD64 qUnequip = 0x140B736EA;
+
+	if (!VirtualProtect((LPVOID)qEquip, 1, PAGE_EXECUTE_READWRITE, &dOldProtect)) return;
+	if (!VirtualProtect((LPVOID)qUnequip, 1, PAGE_EXECUTE_READWRITE, &dOldProtect)) return;
+
+	*(BYTE*)qEquip = 0x30;
+	*(BYTE*)qUnequip = 0x30;
+
+	if (!VirtualProtect((LPVOID)qEquip, 1, dOldProtect, &dOldProtect)) return;
+	if (!VirtualProtect((LPVOID)qUnequip, 1, dOldProtect, &dOldProtect)) return;
+
+	return;
+}
+
+VOID CGameHook::RemoveSpellsRequirements() {
+
+	DWORD processId = GetCurrentProcessId();
+	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, processId);
+
+	std::vector<unsigned int> offsets = { 0x460, 0x68, 0x68, 0x00 };
+	uintptr_t magicAddr = FindExecutableAddress(0x4782838, offsets); //Param + Magic
+	
+	uintptr_t countAddr = magicAddr + 0x0A;
+	int count = 0;
+	ReadProcessMemory(hProcess, (BYTE*)countAddr, &count, sizeof(char) * 2, nullptr);
+
+	for (int i = 0; i < count; i++) {
+		uintptr_t IDOAddr = magicAddr + 0x48 + 0x18 * i;
+		int IDOBuffer;
+		ReadProcessMemory(hProcess, (BYTE*)IDOAddr, &IDOBuffer, sizeof(IDOBuffer), nullptr);
+
+		uintptr_t spellAddr = magicAddr + IDOBuffer + 0x1E; //Intelligence
+		BYTE newValue = 0x00;
+		WriteProcessMemory(hProcess, (BYTE*)spellAddr, &newValue, sizeof(newValue), nullptr);
+
+		spellAddr = magicAddr + IDOBuffer + 0x1F;	//Faith
+		WriteProcessMemory(hProcess, (BYTE*)spellAddr, &newValue, sizeof(newValue), nullptr);
+	}
+
+	return;
+}
+
+VOID CGameHook::RemoveEquipLoad() {
+
+	DWORD processId = GetCurrentProcessId();
+	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, processId);
+
+	std::vector<unsigned int> offsets = { };
+	uintptr_t equipLoadAddr = FindExecutableAddress(0x581FCD, offsets); //EquipLoad 
+	
+	BYTE newValue[4] = {0x0F, (BYTE)0x57, 0xF6, 0x90};
+	WriteProcessMemory(hProcess, (BYTE*)equipLoadAddr, &newValue, sizeof(BYTE) * 4, nullptr);
+
+	return;
+}
+
+BYTE* CGameHook::findPattern(BYTE* pBaseAddress, BYTE* pbMask, const char* pszMask, size_t nLength) {
+	auto DataCompare = [](const BYTE* pData, const BYTE* mask, const char* cmask, BYTE chLast, size_t iEnd) -> bool {
+		if (pData[iEnd] != chLast) return false;
+		for (; *cmask; ++cmask, ++pData, ++mask) {
+			if (*cmask == 'x' && *pData != *mask) {
+				return false;
+			}
+		}
+
+		return true;
+	};
+
+	auto iEnd = strlen(pszMask) - 1;
+	auto chLast = pbMask[iEnd];
+
+	auto* pEnd = pBaseAddress + nLength - strlen(pszMask);
+	for (; pBaseAddress < pEnd; ++pBaseAddress) {
+		if (DataCompare(pBaseAddress, pbMask, pszMask, chLast, iEnd)) {
+			return pBaseAddress;
+		}
+	}
+
+	return nullptr;
+}
+
+BOOL CGameHook::checkIsDlcOwned() {
+	BOOL ret = false;
+
+	int executableSize = 100093 * 1000;
+	BYTE* patternAddr = findPattern((BYTE*)GetModuleBaseAddress(), (BYTE*)csDlcPattern, csDlcMask, executableSize);
+	if (patternAddr != nullptr) {
+		DWORD processId = GetCurrentProcessId();
+		HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, processId);
+
+		int thirdInteger = -1;
+		SIZE_T flag;
+		ReadProcessMemory(hProcess, (BYTE*)(patternAddr + 3), &thirdInteger, sizeof(thirdInteger), &flag);
+		patternAddr = patternAddr + thirdInteger + 7;
+		CSDlc = (uintptr_t)patternAddr;
+
+		std::vector<unsigned int> DLC_1_Offsets = { 0x11 };
+		uintptr_t dlc_1_addr = FindDMAAddyStandalone((uintptr_t)CSDlc, DLC_1_Offsets);
+
+		std::vector<unsigned int> DLC_2_Offsets = { 0x12 };
+		uintptr_t dlc_2_addr = FindDMAAddyStandalone((uintptr_t)CSDlc, DLC_2_Offsets);
+
+		BYTE isDlc_1 = 0x05;
+		ReadProcessMemory(hProcess, (BYTE*)dlc_1_addr, &isDlc_1, sizeof(BYTE), nullptr);
+
+		BYTE isDlc_2 = 0x05;
+		ReadProcessMemory(hProcess, (BYTE*)dlc_2_addr, &isDlc_2, sizeof(BYTE), nullptr);
+
+		if (isDlc_1 == 1 && isDlc_2 == 1) {
+			ret = true;
+		} else {
+			printf("Missing DLC!\n DLC #1 : %d , DLC #2 : %d\n", isDlc_1, isDlc_2);
+		}
+	}
+
+	return ret;
+}
---
+++ archipelago-client/GameHook.h
@@ -0,0 +1,161 @@
+#pragma once
+#include "Core.h"
+#include "./subprojects/minhook/include/MinHook.h"
+
+#define ItemType_Weapon 0
+#define ItemType_Protector 1
+#define ItemType_Accessory 2
+#define ItemType_Goods 4
+
+struct SEquipBuffer;
+
+typedef VOID fEquipItem(DWORD dSlot, SEquipBuffer* E);
+
+class CGameHook {
+public:
+	virtual BOOL preInitialize();
+	virtual BOOL initialize();
+	virtual BOOL updateRuntimeValues();
+	virtual VOID giveItems();
+	virtual VOID itemGib(DWORD itemId);
+	virtual BOOL isSoulOfCinderDefeated();
+	virtual VOID manageDeathLink();
+	virtual BYTE* findPattern(BYTE* pBaseAddress, BYTE* pbMask, const char* pszMask, size_t nLength);
+	int healthPoint = -1, lastHealthPoint = -1, playTime = -1;
+	char soulOfCinderDefeated;
+	SIZE_T healthPointRead, playTimeRead, soulOfCinderDefeatedFlagRead;
+
+	DWORD dIsAutoEquip;
+	DWORD dLockEquipSlots;
+	DWORD dIsNoWeaponRequirements;
+	DWORD dIsNoSpellsRequirements;
+	DWORD dIsNoEquipLoadRequirements;
+	DWORD dIsDeathLink;
+	DWORD dEnableDLC;
+	UINT_PTR qLocalPlayer = 0x144740178;
+	UINT_PTR qWorldChrMan = 0x144768E78;
+	UINT_PTR qSprjLuaEvent = 0x14473A9C8;
+	HANDLE hHeap;
+
+	BOOL deathLinkData = false;
+
+private:
+	static BOOL replaceShellCodeAddress(BYTE* shellcode, int shellCodeOffset, LPVOID codeCave, int codeCaveOffset, int length);
+	static LPVOID InjectShellCode(LPVOID address, BYTE* shellCode, size_t len);
+	static void ConvertToLittleEndianByteArray(uintptr_t address, char* output);
+	static uintptr_t FindExecutableAddress(uintptr_t ptrOffset, std::vector<unsigned int> offsets);
+	static uintptr_t GetModuleBaseAddress();
+	static uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> offsets);
+	static uintptr_t FindDMAAddyStandalone(uintptr_t ptr, std::vector<unsigned int> offsets);
+	static BOOL Hook(DWORD64 qAddress, DWORD64 qDetour, DWORD64* pReturn, DWORD dByteLen);
+	static VOID LockEquipSlots();
+	static VOID RemoveSpellsRequirements();
+	static VOID RemoveEquipLoad();
+	static VOID killThePlayer();
+
+	uintptr_t BaseB = -1;
+	uintptr_t GameFlagData = -1;
+	uintptr_t Param = -1;
+	uintptr_t EquipLoad = -1;
+
+	uintptr_t BaseA = -1;
+	const char* baseAPattern = reinterpret_cast<const char*>("\x48\x8B\x05\x00\x00\x00\x00\x48\x85\xc0\x00\x00\x48\x8b\x40\x00\xc3");
+	const char* baseAMask = "xxx????xxx??xxx?x";
+
+	uintptr_t CSDlc = -1;
+	const char* csDlcPattern = reinterpret_cast<const char*>("\x48\x8B\x0d\x00\x00\x00\x00\x48\x85\xc9\x0f\x84\x00\x00\x00\x00\x0f\xba\xe0\x00\x72\x65\x0f\xba\xe8");
+	const char* csDlcMask = "xxx????xxxxx????xxx?xxxxx";
+
+
+
+	BYTE ItemGibDataShellcode[17] =
+	{
+		0x01, 0x00,
+		0x00, 0x00,
+		0xF4,
+		0x01, 0x00,
+		0x40, 0xFF,
+		0xFF,
+		0xFF,
+		0xFF, 0x00,
+		0x00, 0x00,
+		0x00, 0x0A
+	};
+
+	BYTE ItemGibShellcode[93] =
+	{
+		0x48, 0x83, 0xEC, 0x48,
+		0x44, 0x8D, 0x44, 0x24, 0x20,
+		0x48, 0x8D, 0x54, 0x24, 0x30,
+		0xA1, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+		0x8B, 0x1C, 0x25, 0xFF, 0xFF, 0xFF, 0xFF,
+		0x8B, 0x34, 0x25, 0xFF, 0xFF, 0xFF, 0xFF,
+		0xC7, 0x02, 0x01, 0x00, 0x00, 0x00,							
+		0x89, 0x72, 0x0C,
+		0x41, 0x89, 0x58, 0x14,
+		0x41, 0x89, 0x40, 0x18,
+		0x48, 0xA1, 0x78, 0x8E, 0x76, 0x44, 0x01, 0x00, 0x00, 0x00,
+		0x48, 0x8B, 0xA8, 0x80, 0x00, 0x00, 0x00,
+		0x48, 0x8B, 0x1D, 0xB2, 0x22, 0x77, 0x04,
+		0x48, 0x8B, 0xCB,
+		0xE8, 0x1A, 0xBA, 0x7D, 0x00,
+		0x48, 0x83, 0xC4, 0x48,
+		0xC3
+	};
+
+};
+
+
+class CItemRandomiser {
+public:
+	virtual VOID RandomiseItem(UINT_PTR qWorldChrMan, UINT_PTR pItemBuffer, UINT_PTR pItemData, DWORD64 qReturnAddress);
+	
+	std::vector<DWORD> pLocationsId = { };
+	std::vector<DWORD> pLocationsAddress = { };
+	std::vector<DWORD> pLocationsTarget = { };
+	std::vector<DWORD> pItemsId = { };
+	std::vector<DWORD> pItemsAddress = { };
+	int pBaseId = 0;
+	std::deque<DWORD> receivedItemsQueue = { };
+	std::list<int64_t> checkedLocationsList = { };
+	//TODO Add progressive items (Souls, Titanite, Resins, etc.)
+	bool enablePathOfTheDragon;
+
+private:
+	int isARandomizedLocation(DWORD dItemID);
+	BOOL isReceivedFromServer(DWORD dItemID);
+	BOOL isProgressiveLocation(DWORD dItemID);
+};
+
+class CAutoEquip {
+public:
+	virtual VOID AutoEquipItem(UINT_PTR pItemBuffer, DWORD64 qReturnAddress);
+	virtual BOOL SortItem(DWORD dItemID, SEquipBuffer* E);
+	virtual BOOL FindEquipType(DWORD dItem, DWORD* pArray);
+	virtual DWORD GetInventorySlotID(DWORD dItemID);
+	virtual VOID LockUnlockEquipSlots(int iIsUnlock);
+	fEquipItem* EquipItem; //0x140AFBBB0
+};
+
+struct SEquipBuffer {
+	DWORD dUn1;
+	DWORD dUn2;
+	DWORD dEquipSlot;
+	char unkBytes[0x2C];
+	DWORD dInventorySlot;
+	char paddingBytes[0x60];
+};
+
+extern "C" DWORD64 qItemEquipComms;
+
+extern "C" DWORD64 rItemRandomiser;
+extern "C" VOID tItemRandomiser();
+extern "C" VOID fItemRandomiser(UINT_PTR qWorldChrMan, UINT_PTR pItemBuffer, UINT_PTR pItemData, DWORD64 qReturnAddress);
+
+extern "C" DWORD64 rAutoEquip;
+extern "C" VOID tAutoEquip();
+extern "C" VOID fAutoEquip(UINT_PTR pItemBuffer, DWORD64 pItemData, DWORD64 qReturnAddress);
+
+extern "C" DWORD64 rNoWeaponRequirements;
+extern "C" VOID tNoWeaponRequirements();
+extern "C" VOID fNoWeaponRequirements(DWORD * pRequirementPtr);
